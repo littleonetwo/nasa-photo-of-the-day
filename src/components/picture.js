751dba0c14e4521f0eee5a25a5f3a503f7d80984
littleonetwo <littleonetwo@gmail.com>
2019-10-25 17:43:43 -0500

    added reactstrap styles

--- src/components/picture.js
+++ src/components/picture.js
@@ -1,35 +1,48 @@
 import React, { useState, useEffect } from "react";
-
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap';
 
 
 const PicOfDay = (props) => {
 
   return(
-    <div className = 'inner-container'>
-      <div className= "date">
-        <h4>Date: {props.date}</h4>
-      </div>
+    <div>
+      <Card>
+        <CardImg top width="100%" src={props.picture} alt= {props.title} />
 
-      <div className= "author-stuff">
-        <h2> {props.title} </h2>
-        <p />
-        <h4> By: {props.auth} </h4>
-      </div>
-
-      <div className= "img-container">
-        <img src= {props.picture} alt= {props.title} />
-        <br />
-        <a href= {props.pictureHD}>HD Version</a>
-      </div>
-
-      <div className= "description">
-        <p> {props.desc}</p>
-      </div>
-
-
-
+        <CardBody>
+          <CardSubtitle>By: {props.date}</CardSubtitle>
+          <CardTitle>{props.title}</CardTitle>
+          <CardSubtitle>By: {props.auth}</CardSubtitle>
+          <CardText>{props.desc}</CardText>
+          <Button href={props.pictureHD}>HD Version</Button>
+        </CardBody>
+      </Card>
     </div>
 
+    // <div className = 'inner-container'>
+    //   <div className= "date">
+    //     <h4>Date: {props.date}</h4>
+    //   </div>
+    //
+    //   <div className= "author-stuff">
+    //     <h2> {props.title} </h2>
+    //     <p />
+    //     <h4> By: {props.auth} </h4>
+    //   </div>
+    //
+    //   <div className= "img-container">
+    //     <img src= {props.picture} alt= {props.title} />
+    //     <br />
+    //     <a href= {props.pictureHD}>HD Version</a>
+    //   </div>
+    //
+    //   <div className= "description">
+    //     <p> {props.desc}</p>
+    //   </div>
+    //
+    //
+    //
+    // </div>
   )
 
 }
